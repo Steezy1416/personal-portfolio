b71f60f76cd828d8e03ef13df1beedaf4ae59520
alexfortin <fortin.alex14@gmail.com>
2022-12-31 11:33:12 -0600

    created the portfolio component and styled it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
+import Portfolio from "./components/portfolio/portfolio";
 
 function App() {
   return (
     <>
       <Nav/>
-      <About/>
+      <Portfolio/>
     </>
   );
 }
---
+++ src/components/portfolio/portfolio.js
@@ -0,0 +1,71 @@
+import react from "react";
+import roundTable from "../../assets/images/roundTable.png"
+import movieTime from "../../assets/images/movieTime.png"
+import weatherDash from "../../assets/images/weatherDash.png"
+import quiz from "../../assets/images/quiz.png"
+import studyGuide from "../../assets/images/studyGuide.png"
+import workDaySchedule from "../../assets/images/workDaySchedule.png"
+import "./portfolio.css"
+
+
+export default function Portfolio() {
+    const projects = [
+        {
+            name: "Round Table",
+            repo: "https://github.com/Steezy1416/round-table",
+            deployed: "https://round-table14.herokuapp.com",
+            image: roundTable
+        },
+        {
+            name: "Movie Time",
+            repo: "https://github.com/v1ct0r14m/group-project-1",
+            deployed: "https://v1ct0r14m.github.io/group-project-1/",
+            image: movieTime
+        },
+        {
+            name: "Weather Dashboard",
+            repo: "https://github.com/Steezy1416/weather-dashboard",
+            deployed: "https://steezy1416.github.io/weather-dashboard/",
+            image: weatherDash
+        },
+        {
+            name: "Code Quiz",
+            repo: "https://github.com/Steezy1416/code-quiz-challenge",
+            deployed: "https://steezy1416.github.io/code-quiz-challenge/",
+            image: quiz
+        },
+        {
+            name: "PreWork Study Guide",
+            repo: "https://github.com/Steezy1416/prework-study-guide",
+            deployed: "https://steezy1416.github.io/prework-study-guide/",
+            image: studyGuide
+        },
+        {
+            name: "Work Day Schedule",
+            repo: "https://github.com/Steezy1416/Work-Day-Scheduler",
+            deployed: "https://steezy1416.github.io/Work-Day-Scheduler/",
+            image: workDaySchedule
+        },
+    ]
+
+    return (
+        <div id="project-container">
+            {
+                projects.map(project => (
+                    <div className="project">
+                        <img src={project.image} className="projectImg" />
+                        <p className="projectName">{project.name}</p>
+                        <div className="btnContainer">
+                            <a href={project.repo} target="_blank" >
+                                <button className="projectBtn">Code</button>
+                            </a>
+                            <a href={project.deployed} target="_blank">
+                                <button className="projectBtn">Open</button>
+                            </a>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
